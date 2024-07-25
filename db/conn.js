b345f5a1713a36f9762388b172e02798e68152c4
Felipe Penha <felipepenha32@gmail.com>
2024-07-25 19:17:40 -0300

    #

--- db/conn.js
+++ db/conn.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose')
-
-async function main(){
-
-    try {
-         await mongoose.connect("mongodb+srv://felipepenha32:h10Ry2bZ0oLWi604@estoqueapi.56ohs0d.mongodb.net/?retryWrites=true&w=majority&appName=EstoqueAPI")
-         
-         console.log("Banco Conectado")
-         
-    } catch (error) {
-        console.log(`Erro: ${error}`)
-    }
-
-
-=======
 require('dotenv').config();
 const mongodbUri=process.env.MONGODB_URI
 
@@ -30,7 +14,6 @@ async function main(){
     }
     
     
->>>>>>> e1fe2dd (v0.1)
 }
 
 module.exports = main
